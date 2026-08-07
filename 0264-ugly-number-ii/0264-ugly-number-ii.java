5b68f84934cbc7882eba5e15fc2086d0573a42b0
Kevin Roshan <pkevinroshan57@gmail.com>
2026-08-07 10:17:11 +0530

    Time: 2 ms (99.69%), Space: 43.3 MB (73.29%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int p[]={2,3,5};
-        PriorityQueue<Long> x=new PriorityQueue<>();
-        HashSet<Long> y= new HashSet<>();
-        x.offer(1L);
-        y.add(1L);
-        long cur=1L;
-        for(int i=0;i<n;i++){
-            cur=x.poll();
-            for(int j:p){
-                long z=cur*j;
-                if(!y.contains(z)){
-                    x.offer(z);
-                    y.add(z);
-                }
-            }
+        int dp[]=new int[n];
+        int i2=0,i3=0,i5=0;
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            int n2=dp[i2]*2;
+            int n3=dp[i3]*3;
+            int n5=dp[i5]*5;
+            dp[i]=Math.min(n2,Math.min(n3,n5));
+            if(dp[i]==n2)i2++;
+            if(dp[i]==n3)i3++;
+            if(dp[i]==n5)i5++;
         }
-        return (int)cur;
+        return dp[n-1];
     }
 }
